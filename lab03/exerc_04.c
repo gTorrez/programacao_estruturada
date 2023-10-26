0c475321a9ba72d33d5c3df1511bb70814127dcd
gTorrez <g.ctorres09@gmail.com>
2023-10-26 00:04:19 -0300

    refactor(exerc04): add procura func

--- lab03/exerc_04.c
+++ lab03/exerc_04.c
@@ -4,15 +4,13 @@
 #define MAX_FRASE 1000
 #define MAX_PALAVRA 30
 
-int main(){
-    int i, j, k, check, len_frase, len_aux;
-    char frase[MAX_FRASE], palavra[MAX_PALAVRA], aux[MAX_PALAVRA];
 
-    printf("Digite uma frase:");
-    fgets(frase, MAX_FRASE, stdin);
+int verifica_palavra(char frase[MAX_FRASE], char palavra[MAX_PALAVRA]){
+    int len_frase, len_aux, check, i, j, k;
+    char aux[MAX_PALAVRA];
 
-    printf("Digite uma palavra:");
-    scanf("%s", &palavra);
+    for(i=0; i<MAX_PALAVRA; i++)
+        aux[i] = NULL;
 
     len_frase = strlen(frase);
     check = 1;
@@ -22,14 +20,14 @@ int main(){
         if((frase[i] < 65) || ((frase[i] > 90) && (frase[i] < 97)) || (frase[i] > 122)){
     
             if(strcmp(aux, palavra) == 0){
-                check += 1;
-                break;
+                check = 0;
+                return check;
             }
             
             len_aux = strlen(aux);
 
             for(j=0; j<len_aux; j++)
-                aux[j] = '\000';
+                aux[j] = NULL;
             k = 0;
         }
         else{
@@ -38,7 +36,22 @@ int main(){
         }
     }
 
-    if(check != 1)
+    return check;
+}
+
+
+int main(){
+    int check;
+    char frase[MAX_FRASE], palavra[MAX_PALAVRA];
+    printf("Digite uma frase:");
+    fgets(frase, MAX_FRASE, stdin);
+
+    printf("Digite uma palavra:");
+    scanf("%s", &palavra);
+
+    check = verifica_palavra(frase, palavra);
+
+    if(check == 0)
         printf("sim");
     else
         printf("nao");
